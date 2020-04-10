774d4e2bf258e613a5cf90bc7d28818c1696c752
ita <>
2020-04-10 13:29:19 +0900

    no message

--- testModel.cs
+++ testModel.cs
@@ -1,11 +1,8 @@
 ﻿using System.Runtime.Serialization;
 
-[DataContract()]
 public　class testTable {
 
-	[DataMember]
 	public int test_id { get; set; }
 
-	[DataMember]
 	public string name { get; set; }
 }
