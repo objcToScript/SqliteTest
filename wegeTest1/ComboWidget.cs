12fcb8ef7a70ca37dc3391da7c238839634bb920
ita <>
2020-04-19 16:42:09 +0900

    ウエッジ追加

---
+++ wegeTest1/ComboWidget.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+namespace wegeTest1 {
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class ComboWidget : Gtk.Bin {
+		public ComboWidget() {
+			this.Build();
+		}
+
+		public string bindingProperyName = "";
+
+		public ComboBox _mkComoboBox() {
+			return combobox15;
+		}
+
+		public ComboBox Combo {
+			get {
+				return combobox15;
+			}
+		}
+
+	}
+}
---
+++ wegeTest1/MainWindow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window {
+	public MainWindow() : base(Gtk.WindowType.Toplevel) {
+		Build();
+
+		ComboBox con = combowidget1.Combo;
+
+	}
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a) {
+		Application.Quit();
+		a.RetVal = true;
+	}
+}
